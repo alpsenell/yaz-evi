1177acf7c3395af30c1cdf0666f99c767c784074
alpsenell <alpsenel@outlook.com>
2025-03-10 00:24:25 +0300

    update credds

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,11 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
-console.log(import.meta.env)
+if (!import.meta.env.VITE_FIREBASE_API_KEY) {
+  throw new Error('VITE_FIREBASE_API_KEY is not defined');
+}
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.MODE === 'development' ? import.meta.env.VITE_FIREBASE_API_KEY : process.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "hotel-booking-afd0d.firebaseapp.com",
   projectId: "hotel-booking-afd0d",
   storageBucket: "hotel-booking-afd0d.firebasestorage.app",
