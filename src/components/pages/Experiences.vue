ebb79b8e231634d0c05f8f3b769c0cdf968a3449
alpsenell <alpsenel@outlook.com>
2025-03-10 09:47:06 +0300

    small updates

--- src/components/pages/Experiences.vue
+++ src/components/pages/Experiences.vue
@@ -11,13 +11,23 @@ import HalfHero from "../organisms/HalfHero.vue";
       rotate
       src="https://yazevi.s3.eu-central-1.amazonaws.com/IMG_6413.MOV"
     >
-      <div class="flex flex-col w-full justify-center items-center gap-8 py-6 px-4 md:p-6">
-        <h1 class="font-montserrat text-2xl md:text-3xl text-center">{{ $t('pages.experiences.title') }}</h1>
+      <div class="flex flex-col w-full justify-center items-center gap-8 px-4 py-6 md:p-20">
+        <h1 class="text-3xl font-raleway font-light">
+          {{ $t('pages.experiences.title') }}
+        </h1>
         <div class="flex flex-col gap-4">
-          <p class="font-raleway text-lg lg:text-xl font-light whitespace-pre-wrap">{{ $t('pages.experiences.firstDescription') }}</p>
-          <p class="font-raleway text-lg lg:text-xl font-light whitespace-pre-wrap">{{ $t('pages.experiences.secondDescription') }}</p>
-          <p class="font-raleway text-lg lg:text-xl font-light whitespace-pre-wrap">{{ $t('pages.experiences.thirdDescription') }}</p>
-          <p class="font-raleway text-lg lg:text-xl font-light whitespace-pre-wrap">{{ $t('pages.experiences.fourthDescription') }}</p>
+          <p class="font-raleway text-base font-light">
+            {{ $t('pages.experiences.firstDescription') }}
+          </p>
+          <p class="font-raleway text-base font-light">
+            {{ $t('pages.experiences.secondDescription') }}
+          </p>
+          <p class="font-raleway text-base font-light">
+            {{ $t('pages.experiences.thirdDescription') }}
+          </p>
+          <p class="font-raleway text-base font-light">
+            {{ $t('pages.experiences.fourthDescription') }}
+          </p>
         </div>
       </div>
     </HalfVideoHero>
